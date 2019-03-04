b767a72c32fa0ff86d11bb5befe8daf45f43b2ba
Veganova <virajnpatil@gmail.com>
2019-03-03 22:05:20 -0800

    Added arrow component with basic react motion animation that has it slide into the screen

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import RightArrow from "./RightArrow";
+import {Motion, spring} from "react-motion";
 
 class App extends Component {
   render() {
     return (
-      <div style={{"background-color":"blue"}}>
-        <RightArrow/>
+      <div>
+        <Motion
+            defaultStyle={{ scale: -1.0 }}
+            style={{ scale: spring(1, { stiffness: 60, damping: 10 }) }}
+        >
+          {interpolatedStyles => {
+            return (
+            <div>
+              <RightArrow scale={interpolatedStyles.scale}/>
+            </div>);
+          } }
+        </Motion>
       </div>
     );
   }
--- src/Components/RightArrow.js
+++ src/Components/RightArrow.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
-import Konva from 'konva';
 import { render } from 'react-dom';
-import { Stage, Layer, Shape } from 'react-konva';
+import { Stage, Layer, Shape, Text } from 'react-konva';
 
 class RightArrow extends Component {
     render() {
+        console.log(this.props.scale)
+        // todo give parent width so that the react grid element can be used for sizing.
         return (
+
             <Stage width={window.innerWidth} height={window.innerHeight}>
                 <Layer>
                     <Shape
+                        ref={node => {
+                            this.arrow = node;
+                        }}
+                        text={"some text"}
+                        scaleX={this.props.scale}
+                        scaleY={1}
                         sceneFunc={(context, shape) => {
+                            context.setAttr('textBaseline', 'ideographic');
+                            context.fillText('abc', 0, 0);
                             context.beginPath();
                             context.moveTo(0,0);
                             context.lineTo(100, 0);
@@ -17,12 +27,26 @@ class RightArrow extends Component {
                             context.lineTo(100, 40);
                             context.lineTo(0, 40);
                             context.closePath();
+
                             // (!) Konva specific method, it is very important
                             context.fillStrokeShape(shape);
+
                         }}
                         fill="#00D2FF"
                         stroke="black"
                         strokeWidth={0}
+                    />
+                    <Text
+                        fontSize={18}
+                        scaleX={this.props.scale}
+                        scaleY={1}
+                        text="Projects"
+                        fontFamily='Roboto'
+                        wrap="char"
+                        align="center"
+                        verticalAlign="middle"
+                        width={100}
+                        height={40}
                     />
                 </Layer>
             </Stage>
